32d779845cad6d8f5fba34bd237376abba67d83e
Przemek <przemyslaw.przybylak2@gmail.com>
2025-04-29 18:31:49 +0200

    implement Hero component into Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <main className="w-full overflow-hidden px-[15%]">
+    <main className="w-full h-screen overflow px-[10%] flex flex-col">
       <Navbar />
-      Hello world
-      <div className="fixed -bottom-[40px] -right-[-200px] w-[600px] h-[600px] bg-[radial-gradient(circle_at_left_bottom,var(--tw-gradient-stops)]from-secondary/15 to-transparent blur-[100px] pointer-events-none -z-50 mix-blend-mode-soft-light"></div>
+      <Hero />
     </main>
   );
 }
