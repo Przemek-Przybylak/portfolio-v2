6279460921413b43fe4a165e0fd3d22418ce5f55
Przemek <przemyslaw.przybylak2@gmail.com>
2025-04-30 13:58:24 +0200

    author image added in Hero coponent, and Button component implement

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,12 +1,14 @@
 "use client";
+import Image from "next/image";
 import { Typewriter } from "react-simple-typewriter";
+import Button from "../Button/Button";
 
 export default function Hero() {
   return (
-    <div className="flex flex-row w-full h-full items-end">
+    <div className="flex flex-row w-full h-full items-end ">
       <section className="flex-1 self-center">
         {/* Kontener nagłówka z gradientem i odpowiednim formatowaniem */}
-        <h2 className="min-h-[180px] leading-tight text-[9vw] md:text-[70px] uppercase font-bold bg-gradient-to-r from-[var(--color-accent)] to-[var(--color-muted)] bg-clip-text text-transparent">
+        <h1 className="min-h-[200px] leading-tight text-[9vw] md:text-[5vw] uppercase font-bold bg-gradient-to-r from-[var(--color-accent)] to-[var(--color-muted)] bg-clip-text text-transparent">
           <span style={{ whiteSpace: "pre-line" }}>
             <Typewriter
               words={["Frontend\nDeveloper"]}
@@ -17,7 +19,7 @@ export default function Hero() {
               delaySpeed={1000}
             />
           </span>
-        </h2>
+        </h1>
         <p className="mt-6 text-3">
           Jestem początkującym frontend developerem z solidnymi podstawami w
           React i silnym naciskiem na dobre praktyki. Obecnie rozwijam swoje
@@ -28,10 +30,16 @@ export default function Hero() {
           pracuję samodzielnie i jestem gotowy dołączyć do zespołu, by rozwijać
           się dalej w środowisku produkcyjnym.
         </p>
-        <button className="mt-4">Sprawdź moje projekty</button>
+        <Button>moje projekty</Button>
       </section>
-      <section className="flex-1">
-        <div>picture</div>
+      <section className="flex-1 flex justify-center self-center ">
+        <Image
+          src="/images/cvPhoto.png"
+          alt="Zdjęcie autora"
+          width={300}
+          height={200}
+          className="rounded-xl border-4 border-white/10 shadow-xl transition-all hover:border-white/20 backdrop-blur-lg"
+        />
       </section>
     </div>
   );
