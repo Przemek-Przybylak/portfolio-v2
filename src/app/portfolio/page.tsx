247542d38e8eb2e2fe011ca50bc2839a0b2a2e80
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-02 14:42:48 +0200

    display Tiles for project list in PortfolioPage

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,16 @@
+import { getProjects } from "./getProject";
 import LoadingAnimation from "./LoadingAnimation";
+import ProjectCard from "./ProjectCard";
 
-export default function PortfolioPage() {
-  return <LoadingAnimation>Content</LoadingAnimation>;
+export default async function PortfolioPage() {
+  const projects = await getProjects();
+  return (
+    <LoadingAnimation>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </LoadingAnimation>
+  );
 }
