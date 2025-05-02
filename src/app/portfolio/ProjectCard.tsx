73690c2f224b7cb266e65769a71ac6dd51445cb9
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-02 14:43:04 +0200

    ProjectCard component create

---
+++ src/app/portfolio/ProjectCard.tsx
@@ -0,0 +1,58 @@
+import { Project } from "@/types/projects";
+import Image from "next/image";
+
+export default function ProjectCard({ project }: { project: Project }) {
+  return (
+    <div className="flex flex-col border p-4 rounded-xl shadow bg-[var(--color-light)]">
+      {/* Obrazek projektu */}
+      {project.imageUrl && (
+        <div className=" h-full mb-4 w-full aspect-video relative rounded-lg overflow-hidden">
+          <Image
+            src={project.imageUrl}
+            alt={project.name}
+            fill
+            className="object-cover"
+          />
+        </div>
+      )}
+
+      {/* Nazwa projektu */}
+      <h2 className="text-xl font-bold mb-2">{project.name}</h2>
+
+      {/* Opis */}
+      <p className="text-gray-700">{project.description}</p>
+
+      {/* Technologie */}
+      <div className="flex flex-wrap gap-2 mt-2 items-space-between">
+        {project.technologies?.map((tech) => (
+          <span
+            key={tech}
+            className="bg-[var(--color-muted)] px-2 py-1 rounded text-sm"
+          >
+            {tech}
+          </span>
+        ))}
+      </div>
+
+      {/* Linki */}
+      <div className="mt-4 flex gap-4 ">
+        <a
+          href={project.repoUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[var(--gradient-green-end)] hover:text-[var(--color-accent)] underline"
+        >
+          GitHub
+        </a>
+        <a
+          href={project.demoUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[var(--gradient-green-end)] hover:text-[var(--color-accent)] underline"
+        >
+          Demo
+        </a>
+      </div>
+    </div>
+  );
+}
