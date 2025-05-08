451a09a0d3d7fd8b9fd9539276e2a7df96230c32
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-08 22:09:02 +0200

    Input component added

--- src/app/contact/Input.tsx
+++ src/app/contact/Input.tsx
@@ -5,19 +5,38 @@ interface InputProps {
   label: string;
   placeholder: string;
   type?: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
 }
 
-export default function Input({ label, placeholder, type }: InputProps) {
+export default function Input({
+  label,
+  placeholder,
+  type,
+  onChange,
+}: InputProps) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={label}>{label}</label>
-      <input
-        className="border-1 border-[var(--color-muted)] rounded-md p-2"
-        type={type}
-        id={label}
-        name={label}
-        placeholder={placeholder}
-      />
+      {type === "text" ? (
+        <textarea
+          className="border-1 border-[var(--color-muted)] rounded-md p-2"
+          required
+          id={label}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      ) : (
+        <input
+          className="border-1 border-[var(--color-muted)] rounded-md p-2"
+          type={type}
+          id={label}
+          name={label}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      )}
     </div>
   );
 }
