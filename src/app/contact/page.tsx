73e9709abf796b27607d41bc73a01b8fc1a54d15
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-12 22:37:39 +0200

    styling in Contact component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,31 +1,30 @@
 import { TiMessageTyping } from "react-icons/ti";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiVercel } from "react-icons/si";
-import Form from "./Form";
+import { MdOutlinePhoneAndroid } from "react-icons/md";
 
 export default function ContactPage() {
   return (
-    <main className="w-full  m-0 py-15 grid grid-cols-2 gap-10">
+    <main className="w-full  m-0 py-15 flex gap-10">
       <section className="flex items-center justify-start flex-col gap-20 uppercase">
         <div
           className="flex w-full justify-evenly flex-col gap-10
        text-2xl"
         >
-          <h1>Chcesz porozmawiać o projekcie?</h1>
-          <p className="inline-flex space-x-2">
-            <span>Pozostańmy w kontakcie </span>
+          <div className="inline-flex space-x-2">
+            <h1>Chcesz porozmawiać o projekcie?</h1>
             <TiMessageTyping className="text-2xl " />
-          </p>
+          </div>
         </div>
-        <div className="w-full  m-0 grid grid-cols-2 grid-rows-2 gap-5">
+        <div className="w-full m-0 grid grid-flow-col grid-rows-2 auto-cols-max gap-5">
           <p>Napisz</p>
-          <p>Odwiedź</p>
           <a
             className="lowercase text-[var(--color-muted)] hover:text-gray-500"
             href="mailto:przemyslaw.przybylak2@gmail.com"
           >
             przemyslaw.przybylak2@gmail.com
           </a>
+          <p>Odwiedź</p>
           <div className="flex space-x-4 text-2xl">
             <a
               href="https://github.com/Przemek-Przybylak"
@@ -49,12 +48,11 @@ export default function ContactPage() {
               <SiVercel className="w-6 h-6 text-black hover:text-gray-800" />
             </a>
           </div>
-        </div>
-      </section>
-      <section>
-        <div>
-          <h2 className="text-2xl">Formularz Kontaktowy</h2>
-          <Form />
+          <p>Zadzoń</p>
+
+          <a href="tel:+48519738092" target="_blank" rel="noopener noreferrer">
+            <MdOutlinePhoneAndroid className="w-6 h-6 text-3xl text-[var(--color-accent)] hover:text-gray-800" />
+          </a>
         </div>
       </section>
     </main>
